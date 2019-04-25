be7d3da4e0eaa11b4b8b975afbedf9d418d90e1c
joejnke <joejnke@gmail.com>
2019-04-26 01:35:07 +0300

    tested the ecddng_object_detection() class if it runs same as the notebook version. Test Passed.

--- ecddng_object_detection.py
+++ ecddng_object_detection.py
@@ -6,17 +6,13 @@ import tarfile
 import tensorflow as tf
 import zipfile
 
-
 from distutils.version import StrictVersion
 from collections import defaultdict
 from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
 
-# This is needed since the notebook is stored in the object_detection folder.
-
 from object_detection.utils import ops as utils_ops
-
 if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
   raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
 
@@ -26,26 +22,29 @@ from object_detection.utils import visualization_utils as vis_util
 
 
 class ecddng_obj_detection():
-    def __init__(self, MODEL_NAME = 'rlc_graph'):
+    def __init__(self, PATH_TO_LABELS, MODEL_PATH ='rlc_graph', NUM_CLASSES=3):
         # What model to download.
-        self.MODEL_NAME = MODEL_NAME
+        self.MODEL_PATH = MODEL_PATH
 
         # Path to frozen detection graph. This is the actual model that is used for the object detection.
-        self.PATH_TO_FROZEN_GRAPH = self.MODEL_NAME + '/frozen_inference_graph.pb'
+        self.PATH_TO_FROZEN_GRAPH = self.MODEL_PATH + '/frozen_inference_graph.pb'
 
         # List of the strings that is used to add correct label for each box.
-        self.PATH_TO_LABELS = os.path.join('training', 'object-detection.pbtxt')
+        self.PATH_TO_LABELS = PATH_TO_LABELS #os.path.join('training', 'object-detection.pbtxt')
 
-        self.NUM_CLASSES = 3
+        self.NUM_CLASSES = NUM_CLASSES#3
 
+        self.detection_graph = None
+
+        self.category_index = None
 
     def download_model(self):
         #TODO
         pass
 
     def load_frozen_tf_model(self):
-        detection_graph = tf.Graph()
-        with detection_graph.as_default():
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
             od_graph_def = tf.GraphDef()
             with tf.gfile.GFile(self.PATH_TO_FROZEN_GRAPH, 'rb') as fid:
                 serialized_graph = fid.read()
@@ -54,15 +53,14 @@ class ecddng_obj_detection():
         #TODO
 
     def load_label_map(self):
-        category_index = label_map_util.create_category_index_from_labelmap(self.PATH_TO_LABELS, use_display_name=True)
-        #TODO
+        self.category_index = label_map_util.create_category_index_from_labelmap(self.PATH_TO_LABELS, use_display_name=True)
 
-    def load_image_into_numpy_array(image):
+    def load_image_into_numpy_array(self, image):
         (im_width, im_height) = image.size
         return np.array(image.getdata()).reshape(
             (im_height, im_width, 3)).astype(np.uint8)
 
-    def run_inference_for_single_image(image, graph):
+    def run_inference_for_single_image(self, image, graph):
         with graph.as_default():
             with tf.Session() as sess:
                 # Get handles to input and output tensors
@@ -106,7 +104,21 @@ class ecddng_obj_detection():
                 output_dict['detection_scores'] = output_dict['detection_scores'][0]
                 if 'detection_masks' in output_dict:
                     output_dict['detection_masks'] = output_dict['detection_masks'][0]
+
         return output_dict
+
+    def visualize_boxes_and_labels_on_image_array(self, detection_output_dict, image_np, fig_size=(12, 8)):
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            detection_output_dict['detection_boxes'],
+            detection_output_dict['detection_classes'],
+            detection_output_dict['detection_scores'],
+            self.category_index,
+            instance_masks=detection_output_dict.get('detection_masks'),
+            use_normalized_coordinates=True,
+            line_thickness=8)
+        plt.figure(figsize=fig_size)
+        return plt.imshow(image_np)
 
     def run_inference_for_multiple_image(self):
         #TODO
@@ -115,6 +127,20 @@ class ecddng_obj_detection():
 
 def run_ecddng_obj_detection():
     #TODO : test the functions in the ecddng_obj_detection() class
+    MODEL_PATH = '/home/kira/cloned/tensorflow object detection API/models/research/object_detection/rlc_graph'
+    PATH_TO_LABELS = '/home/kira/cloned/tensorflow object detection API/models/research/object_detection/training/object-detection.pbtxt'
+    image_path = '/home/kira/cloned/tensorflow object detection API/models/research/object_detection/test_images/image3.jpg'
+
+    try_ecddng= ecddng_obj_detection(MODEL_PATH=MODEL_PATH, PATH_TO_LABELS=PATH_TO_LABELS,NUM_CLASSES=3)
+    try_ecddng.load_frozen_tf_model()
+    try_ecddng.load_label_map()
+
+    image_np = try_ecddng.load_image_into_numpy_array(Image.open(image_path))
+
+    output_dict=try_ecddng.run_inference_for_single_image(image=image_np, graph= try_ecddng.detection_graph)
+    try_ecddng.visualize_boxes_and_labels_on_image_array(detection_output_dict= output_dict, image_np= image_np)
+    plt.show()
+
     pass
 
 
